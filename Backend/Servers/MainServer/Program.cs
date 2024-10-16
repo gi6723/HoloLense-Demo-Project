c538e651b0adf1e9e54410d8d75a63e5a0132e14
Gianni Ioannou <108948908+gi6723@users.noreply.github.com>
2024-10-16 01:49:59 -0500

    Added in basic functionality so I can test if the forwarding from middleman to main works as it should

--- Backend/Servers/MainServer/Program.cs
+++ Backend/Servers/MainServer/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WatsonWebsocket;
+using System;
+
+class Program
+{
+    static WatsonWsServer wsServer;
+
+    static void Main(string[] args)
+    {
+        string serverIp = "100.121.5.87";  // IP of the MainServer (VM)
+        int serverPort = 8181;             // Port the MainServer will listen on
+
+        // Start the WebSocket server
+        wsServer = new WatsonWsServer(serverIp, serverPort, false);
+        wsServer.ClientConnected += ClientConnected;
+        wsServer.MessageReceived += MessageReceived;
+        wsServer.Start();
+
+        Console.WriteLine($"Main WebSocket server started on {serverIp}:{serverPort}");
+        Console.ReadLine();  // Keep the server running
+    }
+
+    static void ClientConnected(object sender, ClientConnectedEventArgs args)
+    {
+        Console.WriteLine($"Client connected: {args.IpPort}");
+    }
+
+    static void MessageReceived(object sender, MessageReceivedEventArgs args)
+    {
+        string receivedData = System.Text.Encoding.UTF8.GetString(args.Data);
+        Console.WriteLine($"Data received from Middleman: {receivedData}");
+    }
+}
+
--- Backend/Servers/MiddlemanServer/Program.cs
+++ Backend/Servers/MiddlemanServer/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WatsonWebsocket;
+using System;
+
+class Program
+{
+    static WatsonWsServer wsMiddleman;
+    static WatsonWsClient wsClientToMainServer;
+
+    static void Main(string[] args)
+    {
+        string middlemanIp = "100.79.34.6";  // IP of the MiddlemanServer (Desktop)
+        int middlemanPort = 8181;            // Port the MiddlemanServer listens on
+
+        string mainServerIp = "100.121.5.87";  // IP of the MainServer (VM)
+        int mainServerPort = 8181;             // Port to forward data to
+
+        // Start Middleman WebSocket Server
+        wsMiddleman = new WatsonWsServer(middlemanIp, middlemanPort, false);
+        wsMiddleman.ClientConnected += ClientConnected;
+        wsMiddleman.MessageReceived += (sender, e) => ForwardToMainServer(e.Data);
+        wsMiddleman.Start();
+
+        // Set up the connection to the MainServer
+        wsClientToMainServer = new WatsonWsClient(new Uri($"ws://{mainServerIp}:{mainServerPort}"));
+        wsClientToMainServer.ServerConnected += (sender, e) => Console.WriteLine("Connected to MainServer");
+        wsClientToMainServer.ServerDisconnected += (sender, e) => Console.WriteLine("Disconnected from MainServer");
+        wsClientToMainServer.Start();
+
+        Console.WriteLine($"Middleman WebSocket server started on {middlemanIp}:{middlemanPort}");
+        Console.ReadLine();  // Keep the server running
+    }
+
+    static void ClientConnected(object sender, ClientConnectedEventArgs args)
+    {
+        Console.WriteLine($"Client connected: {args.IpPort}");
+    }
+
+    static void ForwardToMainServer(byte[] data)
+    {
+        string message = System.Text.Encoding.UTF8.GetString(data);
+        Console.WriteLine($"Received from client, forwarding to MainServer: {message}");
+
+        if (wsClientToMainServer != null && wsClientToMainServer.Connected)
+        {
+            wsClientToMainServer.Send(data);
+            Console.WriteLine("Data forwarded to MainServer");
+        }
+        else
+        {
+            Console.WriteLine("Failed to forward, MainServer is not connected.");
+        }
+    }
+}
+
